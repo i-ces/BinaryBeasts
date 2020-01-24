bc9d75cc99135184786cb20777301856ca7c7bda
subash sharma <subashsharma2628@gmail.com>
2020-01-23 22:59:16 -0800

    11PM

--- client/javascript/first.js
+++ client/javascript/first.js
@@ -17,24 +17,65 @@ var insertHtml = function (selector, html) {
 
 
 var homeUrl = "snippets/home.html";
+var registerurl = "snippets/registration.html";
+
 //window.location = homeUrl;
  //$("#main-content").load(homeUrl);
 $(function(){
     
-    showLoading("#main-content");
+    showLoading("#main-content"); //show loading gif
 
-    $ajaxUtils.sendGetRequest(
-        homeUrl,
-        function(home_html){
-            insertHtml("#main-content",home_html);
-        },
-        false    
-)});
+//     $ajaxUtils.sendGetRequest(
+//         homeUrl,
+//         function(home_html){
+//             insertHtml("#main-content",home_html);
+//         },
+//         false   //no json response 
+// )
+	
+	$("#main-content").load(homeUrl);
+
+});
 
 
 
 //console.log("Entered!!!");
 
+
+dc.getRegister = function(){
+	    showLoading("#main-content");
+
+	    // $ajaxUtils.sendGetRequest(
+	    // 		registerurl , 
+	    // 		function(register_page){
+	    // 			insertHtml("#main-content", register_page);
+	    // 		},
+	    // 		false  //no json response
+	    // 	);
+	    	$("#main-content").load(registerurl);
+
+
+
+};
+
+dc.validateForm = function(){
+	//showLoading("#main-content");
+	// $("#main-content").load(registerurl);
+			//alert("entered");
+
+	 		  var x = document.getElementById("Citizenship").value;
+              var y = document.getElementById("location").value;
+              var z = document.getElementById("chheckbox").checked;              
+              var a = x.length;
+              var b = y.length;
+              if (a == 14 && b == 3 && z ){
+                alert("you are registered");
+              	}
+              else{
+                alert("wrong input");
+              	}
+
+};
 
 
 

56bcef3c1aadf624900254bf21f71e40ae451b4e
subash sharma <subashsharma2628@gmail.com>
2020-01-24 09:32:37 -0800

    9:30AM

--- client/javascript/first.js
+++ client/javascript/first.js
@@ -19,13 +19,15 @@ var insertHtml = function (selector, html) {
 var homeUrl = "snippets/home.html";
 var registerUrl = "snippets/registration.html";
 var statusUrl = "snippets/statusCheck.html";
-
-
+var dashBoardUrl = "snippets/dashBoard.html";
+var checkerUrl = "snippets/checker.html";
+var resultUrl = "snippets/result.html";
+var helpUrl ="snippets/help.html";
 //window.location = homeUrl;
  //$("#main-content").load(homeUrl);
 $(function(){
     
-    //showLoading("#main-content"); //show loading gif
+    showLoading("#main-content"); //show loading gif
 
 //     $ajaxUtils.sendGetRequest(
 //         homeUrl,
@@ -45,7 +47,7 @@ $(function(){
 
 
 dc.getRegister = function(){
-	    //showLoading("#main-content");
+	    showLoading("#main-content");
 
 	    // $ajaxUtils.sendGetRequest(
 	    // 		registerurl , 
@@ -61,10 +63,82 @@ dc.getRegister = function(){
 };
 
 dc.getStatus = function(){
-    //showLoading("#main-content");
+    showLoading("#main-content");
 
 	$("#main-content").load(statusUrl);
     
+};
+
+
+dc.getDashboard = function(){
+	showLoading("#main-content");
+	$("#main-content").load(dashBoardUrl);
+
+};
+
+dc.getChecker = function(){
+	showLoading("#main-content");
+
+
+	$("#main-content").load(checkerUrl);
+
+
+	 $(function() {
+                                              const z = prompt("Enter date of birth in the order year/month/day. For eg. 2055/11/15");
+                                              const m = z.split('/').join('');
+                                              const n = m.substring(0,4);
+                                              const o = m.substring(4,6);
+                                              const p = m.substring(6,8);
+                                              const a = parseInt(n);
+                                              const datetoday = 20751010;
+                                              const age = (datetoday - parseInt(m))/10000;
+                                              alert(".............");
+                                              const x = document.getElementById("gg");
+                                              const y = document.getElementById("ff");
+                                              if(a < 2075 && a > 1960 && parseInt(o)<13 && parseInt(p)<32) { 
+                                                
+                                                if(age <18){
+                                                      //console.log("Entered!!!");
+                                                      y.style.color= "Red"; 
+                                                      x.style.backgroundColor= "#FF5D52"; 
+
+                                                      
+                                                      y.innerHTML= "Age below 18"; 
+                                                
+                                                    }
+                                                      
+                                                  //y.innerHTML= "Finger print not found!!"; 
+                                                    
+                                                    else{
+                                                   console.log(".............");
+                                                  y.style.color= "green"; 
+                                                  y.innerHTML= "All Ok!"; 
+                                                  x.style.backgroundColor= "lightgreen"; 
+
+                                                      }
+                                                }
+                                            else
+                                            {
+                                                alert("Wrong input!!! ");
+                                            }   
+                                                
+                                                
+    //{console.log("kick back!!!");}
+});
+
+};
+
+dc.getResult = function(){
+		showLoading("#main-content");
+			$("#main-content").load(resultUrl);
+
+};
+
+
+dc.getHelp = function(){
+			showLoading("#main-content");
+			$("#main-content").load(helpUrl);
+
 };
 
 
